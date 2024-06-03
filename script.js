400886f35ebbce09f688b5ab6811c5ff28b9dafb
Sushmithaasri K N <156002335+Sushmithaasri-2005@users.noreply.github.com>
2024-06-03 14:47:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+const btn=document.getElementById("button")
+const hexcode=document.getElementById("hexcode")
+const bg=document.getElementById("background")
+const hex=[0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+btn.addEventListener('click',flipBG)
+function flipBG()
+{
+    let hexcolor='#'
+    for(let i=0;i<6;i++)
+    {
+        hexcolor+=randomHex()
+    }
+    console.log(hexcolor)
+    bg.style.backgroundColor=hexcolor
+    hexcode.innerHTML=hexcolor
+
+}
+function randomHex()
+{
+    let randomIndex=Math.floor(Math.random()*16)
+    return hex[randomIndex]
+}
 
